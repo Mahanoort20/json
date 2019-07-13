304c00dde2a881025518b67c3cba11830d6b8366
Mahanoort20 <49521284+Mahanoort20@users.noreply.github.com>
2019-07-13 17:20:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,580 @@
+let contact1 = {
+    name: "Mahanoor",
+    number: "03009256560",
+    lastSeen: "2pm",
+    message: "Hey! Whats'up?",
+    status: "can't talk what's app only"
+
+}
+let contact2 = {
+    name: "Uzma",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "busy"
+}
+let contact3 = {
+    name: "Kainat",
+    number: "030098787",
+    lastSeen: "Yesterday",
+    message: "Hey! Whats'up?",
+    status: "available"
+}
+let contact4 = {
+    name: "Farheen",
+    number: "03009255464",
+    lastSeen: "3pm",
+    message: "Hey! Whats'up?",
+    status: "at gym"
+}
+let contact5={
+    name: "Sarah",
+    number: "03009256560",
+    lastSeen: "2pm",
+    message: "Hey! Whats'up?",
+    status: "at movie"
+}
+let contact6 = {
+    name: "Irma",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "at work"
+}
+let contact7 = {
+    name: "Samah",
+    number: "030098787",
+    lastSeen: "Yesterday",
+    message: "Hey! Whats'up?",
+    status: "sleeping"
+}
+let contact8 = {
+    name: "Saima",
+    number: "03009255464",
+    lastSeen: "3pm",
+    message: "Hey! Whats'up?",
+    status: "sad"
+}
+let contact9 = {
+    name: "Sadia",
+    number: "03009255464",
+    lastSeen: "3pm",
+    message: "Hey! Whats'up?",
+    status: "do not distrub"
+}
+let contact10={
+    name: "Noor",
+    number: "03009256560",
+    lastSeen: "2pm",
+    message: "Hey! Whats'up?",
+    status: "do not distrub"
+
+}
+let contact11 = {
+    name: "Misbah",
+    number: "03009256560",
+    lastSeen: "2pm",
+    message: "Hey! Whats'up?",
+    status: "sad"
+}
+let contact12 = {
+    name: "Sehrish",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "sleeping"
+}
+let contact13 = {
+    name: "Tuba",
+    number: "030098787",
+    lastSeen: "Yesterday",
+    message: "Hey! Whats'up?",
+    status: "busy"
+}
+let contact14 = {
+    name: "Ali",
+    number: "03009255464",
+    lastSeen: "3pm",
+    message: "Hey! Whats'up?",
+    status: "eid mubarak"
+}
+let contact15={
+    name: "Nida",
+    number: "03009256560",
+    lastSeen: "2pm",
+    message: "Hey! Whats'up?",
+    status: "in a meeting"
+}
+let contact16 = {
+    name: "Moiz",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "at gym"
+}
+let contact17 = {
+    name: "Farhan",
+    number: "030098787",
+    lastSeen: "Yesterday",
+    message: "Hey! Whats'up?",
+    status: "at work"
+}
+let contact18 = {
+    name: "Aliza",
+    number: "03009255464",
+    lastSeen: "3pm",
+    message: "Hey! Whats'up?",
+    status: "allhumduillah"
+}
+let contact19 = {
+    name: "Arham",
+    number: "03009255464",
+    lastSeen: "3pm",
+    message: "Hey! Whats'up?",
+    status: "available"
+}
+let contact20={
+    name: "Bisma",
+    number: "03009256560",
+    lastSeen: "2pm",
+    message: "Hey! Whats'up?",
+    status: "argent calls only"
+
+}
+let contact21 = {
+    name: "Feroz",
+    number: "03009256560",
+    lastSeen: "2pm",
+    message: "Hey! Whats'up?",
+    status: "available"
+}
+let contact22 = {
+    name: "Dua",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "argent calls only"
+}
+let contact23 = {
+    name: "Sajal",
+    number: "030098787",
+    lastSeen: "Yesterday",
+    message: "Hey! Whats'up?",
+    status: "at the movies"
+}
+let contact24 = {
+    name: "Saba",
+    number: "03009255464",
+    lastSeen: "3pm",
+    message: "Hey! Whats'up?",
+    status: "argent calls only"
+}
+let contact25={
+    name: "Faizan",
+    number: "03009256560",
+    lastSeen: "2pm",
+    message: "Hey! Whats'up?",
+    status: "at the movies"
+}
+let contact26 = {
+    name: "Maria",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "argent calls only"
+}
+let contact27 = {
+    name: "Asad",
+    number: "030098787",
+    lastSeen: "Yesterday",
+    message: "Hey! Whats'up?",
+    status: "Hey! There I am using whatsapp"
+}
+let contact28 = {
+    name: "Ahmed",
+    number: "03009255464",
+    lastSeen: "3pm",
+    message: "Hey! Whats'up?",
+    status:"busy"
+}
+let contact29 = {
+    name: "Nabeel",
+    number: "03009255464",
+    lastSeen: "3pm",
+    message: "Hey! Whats'up?",
+    status:"Hey! There I am using whatsapp"
+}
+let contact30={
+    name: "Umair",
+    number: "03009256560",
+    lastSeen: "2pm",
+    message: "Hey! Whats'up?",
+    status: "busy"
+
+}
+let contact31 = {
+    name: "Sameer",
+    number: "03009256560",
+    lastSeen: "2pm",
+    message: "Hey! Whats'up?",
+    status: "busy"
+}
+let contact32 = {
+    name: "Ahmer",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "Hey! There I am using whatsapp"
+}
+let contact33 = {
+    name: "Hasan",
+    number: "030098787",
+    lastSeen: "Yesterday",
+    message: "Hey! Whats'up?",
+    status: "sleeping"
+}
+let contact34 = {
+    name: "Saqib",
+    number: "03009255464",
+    lastSeen: "3pm",
+    message: "Hey! Whats'up?",
+    status: "studing"
+}
+let contact35={
+    name: "Nimra",
+    number: "03009256560",
+    lastSeen: "2pm",
+    message: "Hey! Whats'up?",
+    status: "sleeping"
+}
+let contact36 = {
+    name: "Saman",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "sleeping"
+    
+}
+let contact37 = {
+    name: "Fahad",
+    number: "030098787",
+    lastSeen: "Yesterday",
+    message: "Hey! Whats'up?",
+    status: "Hey! There I am using whatsapp"
+}
+let contact38 = {
+    name: "Mariyam",
+    number: "03009255464",
+    lastSeen: "3pm",
+    message: "Hey! Whats'up?",
+    status: "busy"
+}
+let contact39 = {
+    name: "Arsalan",
+    number: "03009255464",
+    lastSeen: "3pm",
+    message: "Hey! Whats'up?",
+    status: "Hey! There I am using whatsapp"
+}
+let contact40={
+    name: "Ifsa",
+    number: "03009256560",
+    lastSeen: "2pm",
+    message: "Hey! Whats'up?"
+
+}
+let contacts =[
+{
+
+
+    name: "Mahanoor",
+    number: "03009256560",
+    lastSeen: "2pm",
+    message: "Hey! Whats'up?",
+    status: "can't talk what's app only"
+
+},
+{
+    name: "Uzma",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "busy"
+},
+{
+    name: "Kainat",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "available"
+},
+{
+    name: "Farheen",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "at gym"
+},
+{
+    name: "Sarah",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "at movie"
+},
+{
+    name: "Irma",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "at work"
+},
+{
+    name: "Samah",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "sleeping"
+},
+{
+    name: "Saima",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "sad"
+
+},
+{
+    name: "Sadia",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "do not distrub"
+},
+{
+    name: "Noor",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "do not distrub"
+},  
+{
+
+
+    name: "Misbah",
+    number: "03009256560",
+    lastSeen: "2pm",
+    message: "Hey! Whats'up?",
+    status: "sad"
+
+},
+{
+    name: "Sehrish",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "sleeping"
+},
+{
+    name: "Tuba",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "busy"
+},
+{
+    name: "Ali",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "eid mubarak"
+},
+{
+    name: "Nida",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "in meeting"
+},
+{
+    name: "Moiz",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "at gym"
+},
+{
+    name: "Farhan",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "at work"
+},
+{
+    name: "Aliza",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "allhumduillah"
+},
+{
+    name: "Arham",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "available"
+},
+{
+    name: "Bisma",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "argent calls only"
+}, 
+{
+
+
+    name: "Feroz",
+    number: "03009256560",
+    lastSeen: "2pm",
+    message: "Hey! Whats'up?",
+    status: "available"
+
+},
+{
+    name: "Dua",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "argent calls only"
+},
+{
+    name: "Sajal",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "at the movies"
+},
+{
+    name: "Saba",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "argent calls only"
+},
+{
+    name: "Faizan",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "at the movies"
+},
+{
+    name: "Maria",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "Hey! There I am using whatsapp"
+},
+{
+    name: "Asad",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "Busy"
+},
+{
+    name: "Ahmed",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "Hey! There I am using whatsapp"
+},
+{
+    name: "Nabeel",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "Busy"
+},
+{
+    name: "Umair",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "Busy"
+},
+{
+
+
+    name: "Sameer",
+    number: "03009256560",
+    lastSeen: "2pm",
+    message: "Hey! Whats'up?",
+    status: "Hey! There I am using whatsapp"
+
+},
+{
+    name: "Ahmer",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "Studying"
+},
+{
+    name: "Hasan",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "Sleeping"
+},
+{
+    name: "Saqib",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "Studying"
+},
+{
+    name: "Nimra",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "Sleeping"
+},
+{
+    name: "Saman",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "Sleeping"
+},
+{
+    name: "Fahad",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "Busy"
+},
+{
+    name: "Mariyam",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "Hey! There I am using whatsapp"
+},
+{
+    name: "Arsalan",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "Busy"
+},
+{
+    name: "Ifsa",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?",
+    status: "Hey! There I am using whatsapp"
+}  
+]
